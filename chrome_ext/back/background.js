b2ce9fac45ac248c152e43a7ecac36a0320c648e
Richard Gong <richardgong@gmail.com>
2017-12-03 09:04:14 -0800

    manage windows/tabs, give focus

---
+++ chrome_ext/back/background.js
@@ -0,0 +1,39 @@
+console.log("history-by-date's background.js loaded")
+
+function openHistoryTab() {
+  var indexUrl = chrome.extension.getURL('/dist/index.html');
+
+  chrome.tabs.query({url: indexUrl}, function(tabs) {
+    if (tabs.length) {
+      var tab = tabs[0];
+      chrome.windows.update(tab.windowId, {focused: true});
+      chrome.tabs.update(tab.id, {active: true});
+    } else {
+      console.log("No tab found:", indexUrl);
+      chrome.tabs.create({url: indexUrl});
+    }
+  })
+}
+
+
+chrome.browserAction.onClicked.addListener(function(activeTab)
+{
+  openHistoryTab()
+});
+
+
+
+
+chrome.commands.onCommand.addListener(function(command) {
+  /**
+   * You need to uninstall and reinstall your local extension to see the default keyboard command appear in
+   *
+   * chrome://extensions/configureCommands
+   *
+   * Even then, you might have to manually set it
+   *
+   */
+  console.log('Command:', command);
+  if (command == 'open-history-tab')
+    reopen()
+});
