7255c31a235fead0758e9c2b76367ee23584f2a8
Richard Gong <richardgong@gmail.com>
2017-12-03 17:51:01 -0800

    promise.reject if undefined values

--- app/Store.js
+++ app/Store.js
@@ -1,52 +1,11 @@
 import { action, observable, computed } from 'mobx'
 
+import { storageGet, storageSet } from './storage.js'
+
+
 export const localKeys = {
   filterEnabled: 'filterEnabled',
   filters: 'filters'
-}
-
-let storageGet, storageSet
-
-if (chrome.storage && chrome.storage.local) {
-  storageGet = key => {
-    return new Promise((resolve, reject) => {
-      chrome.storage.local.get(key, items => {
-        if (chrome.runtime.lastError)
-          return reject(chrome.runtime.lastError)
-        let value = items[key]
-        console.log("Storage.get:", key, value)
-        if (value != null)
-          value = JSON.parse(value)
-        return resolve(value)
-      })
-    })
-  }
-
-  storageSet = (key, object) => {
-    let keyValue = {[key]: JSON.stringify(object)}
-    console.log("Storage.set:", keyValue)
-    return new Promise((resolve, reject) => {
-      chrome.storage.local.set(keyValue,
-        () => {
-          if (chrome.runtime.lastError)
-            return reject(chrome.runtime.lastError)
-          return resolve(true)
-        })
-    })
-  }
-} else {
-  console.warn("Chrome storage not available; using localStorage")
-
-  storageGet = key => {
-    let item = localStorage.getItem(key)
-    if (item == null)
-      return Promise.reject("Not found in localStorage: " + key)
-    return Promise.resolve(JSON.parse(item))
-  }
-
-  storageSet = (key, object) => {
-    return Promise.resolve(localStorage.setItem(key, JSON.stringify(object)))
-  }
 }
 
 export default class Store {
@@ -67,17 +26,12 @@ export default class Store {
 
 @action
   setFilterEnabled_(value) {
-    if (value == undefined)
-      return
     this.filterEnabled = value
     storageSet(localKeys.filterEnabled, value)
   }
 
 @action
   setFilters_(rawFilters) {
-    if (rawFilters == undefined)
-      return
-
     let filterMap = {}
     let filters = []
     for (let filter of rawFilters) {
---
+++ app/storage.js
@@ -0,0 +1,46 @@
+
+
+
+if (chrome.storage && chrome.storage.local) {
+  window.storageGet = key => {
+    return new Promise((resolve, reject) => {
+      chrome.storage.local.get(key, items => {
+        if (chrome.runtime.lastError)
+          return reject(chrome.runtime.lastError)
+        let value = items[key]
+        console.log("Storage.get:", key, value)
+        if (value == null)
+          return reject("Not found in Chrome storage: " + key)
+        return resolve(JSON.parse(value))
+      })
+    })
+  }
+
+  window.storageSet = (key, object) => {
+    let keyValue = {[key]: JSON.stringify(object)}
+    console.log("Storage.set:", keyValue)
+    return new Promise((resolve, reject) => {
+      chrome.storage.local.set(keyValue,
+        () => {
+          if (chrome.runtime.lastError)
+            return reject(chrome.runtime.lastError)
+          return resolve(true)
+        })
+    })
+  }
+} else {
+  console.warn("Chrome storage not available; using localStorage")
+
+  window.storageGet = key => {
+    let item = localStorage.getItem(key)
+    if (item == null)
+      return Promise.reject("Not found in localStorage: " + key)
+    return Promise.resolve(JSON.parse(item))
+  }
+
+  window.storageSet = (key, object) => {
+    return Promise.resolve(localStorage.setItem(key, JSON.stringify(object)))
+  }
+}
+
+export {storageGet, storageSet}
